6f27add968fa4ecb2f54cb0202e50f88677f1f45
onjoku <49341640+onjoku@users.noreply.github.com>
2019-09-27 11:34:05 -0400

    Program to show that Github account has been created successfully.

---
+++ hello1.py
@@ -0,0 +1,13 @@
+""" Program to displays Hello world
+    Author: Ogadinma Njoku
+    Purpose: To show that a github account has been successfully created
+    for Course 567 -WS
+"""
+
+def hellow_world():
+
+    #print hello world
+    return "Hello world!"
+
+hellow_world()
+
